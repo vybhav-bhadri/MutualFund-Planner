a076d9ce1ad8aaaf17c18eb2651f5c44422394ee
vybhav <svbhadri1110@gmail.com>
2021-06-13 00:54:37 +0530

    csrf and secret key

--- fundcalulator/__init__.py
+++ fundcalulator/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app=Flask(__name__)
 
-app.config['SECRET_KEY']='36e47e82246707aa45b6e668a89605a6'
+csrf = CSRFProtect(app)
+
+app.config['SECRET_KEY']='6e9ad540bfd3c08814f6b51f44150875'
 
 from fundcalulator import routes
