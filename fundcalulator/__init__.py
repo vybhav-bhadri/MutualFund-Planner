c7619b98697264ff4ebfd2ed9467f6366adeda32
vybhav <svbhadri1110@gmail.com>
2021-06-13 13:54:25 +0530

    csrf input

--- fundcalulator/__init__.py
+++ fundcalulator/__init__.py
@@ -4,6 +4,7 @@ from flask_wtf.csrf import CSRFProtect
 app=Flask(__name__)
 
 # csrf = CSRFProtect(app)
+#<!-- <input type="hidden" name="csrf_token" value="{{ csrf_token() }}"/> -->
 
 app.config['SECRET_KEY']='6e9ad540bfd3c08814f6b51f44150875'
 # app.config['SESSION_COOKIE_SECURE'] = False
